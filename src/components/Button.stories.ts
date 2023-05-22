0adfc139a69373fd70577627fdc41048e81b8fb5
Phat <nguyenphatit.nl@gmail.com>
2023-05-22 18:08:49 +0700

    Add test to storybook

--- src/components/Button.stories.ts
+++ src/components/Button.stories.ts
@@ -9,6 +9,8 @@ const meta: Meta<typeof Button> = {
      */
     title: 'Button',
     component: Button,
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
 };
 
 export default meta;
