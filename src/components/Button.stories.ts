9c5ca0060f50d924d02262460e19f54d30706dee
Phat <nguyenphatit.nl@gmail.com>
2023-05-22 17:51:56 +0700

    first commit

---
+++ src/components/Button.stories.ts
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from './Button';
+
+const meta: Meta<typeof Button> = {
+    /* 👇 The title prop is optional.
+     * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+     * to learn how to generate automatic titles
+     */
+    title: 'Button',
+    component: Button,
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+    args: {
+        primary: true,
+        label: 'Button',
+    },
+}
+
+export const Secondary: Story = {
+    args: {
+        label: 'Button',
+    }
+}
+
+export const Large: Story = {
+    args: {
+        size: 'large',
+        label: 'Button',
+    },
+};
+
+export const Small: Story = {
+    args: {
+        size: 'small',
+        label: 'Button',
+    },
+};
---
+++ src/components/Button.tsx
@@ -0,0 +1,70 @@
+import React, { useMemo } from 'react';
+
+interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+const getSizeClasses = (size: "small" | "medium" | "large") => {
+  switch (size) {
+    case 'small': {
+      return 'px-4 py-2.5';
+    }
+    case 'large': {
+      return 'px-6 py-3';
+    }
+    default: {
+      return 'px-5 py-2.5';
+    }
+  }
+}
+
+const getModeClasses = (isPrimary: boolean) =>
+  isPrimary
+    ? 'text-white bg-pink-600 border-pink-600'
+    : 'text-pink-600 bg-transparent border-pink-600';
+
+const BASE_BUTTON_CLASSES =
+    'cursor-pointer rounded-full border-2 font-bold leading-none inline-block';
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({
+  primary = false,
+  size = 'medium',
+  label,
+  ...props
+}: ButtonProps) => {
+
+  const computedClasses = useMemo(() => {
+    const modeClass = getModeClasses(primary);
+    const sizeClass = getSizeClasses(size);
+
+    return [modeClass, sizeClass].join(' ');
+  }, [primary, size]);
+
+  return (
+    <button type="button" className={`${BASE_BUTTON_CLASSES} ${computedClasses}`} {...props}>
+      {label}
+    </button>
+  );
+};
